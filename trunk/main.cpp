798a4f6a2e052a22e2c866871728951b7c16edf4
v.kirkizh <v.kirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-04-28 08:57:57 +0000

    first QT steps

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,15 +1,9 @@
-#include "experiment.h"
+#include <QtGui>
 
 // entry point
-int main () {
-	BOARD board;
-	std::cout << board << std::endl;
+int main (int argc, char** argv) {
+	QApplication shashki(argc, argv);
 	
-	EXPERIMENT exp(board, PWHITE, 50, 1000, 0.10, 5, 7);
-	exp.run(AI_PRL); // 2 threads
-	exp.run(AI_SEQ); // sequence
-	exp.run(AI_PRL2); // 4 threads
-	
-	return 0;
+	return shashki.exec();
 }
 
