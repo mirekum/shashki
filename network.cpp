876441fec040d8654d9ead20122844918b551672
k3byh83w2pdcvw <k3byh83w2pdcvw@mail.ru>
2010-07-06 11:11:31 +0000

    Netvork player change

--- network.cpp
+++ network.cpp
@@ -11,3 +11,105 @@ MOVE NETWORK_PLAYER::getMove(BOARD board) {
 	return result;
 }
 
+
+bool NETWORK_PLAYER::create_Server()
+{
+
+      struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strcpy(ifr.ifr_name, "eth0");
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    ioctl(sock, SIOCGIFADDR, &ifr);
+    close(sock);
+
+    struct sockaddr_in *sa = (struct sockaddr_in*)&ifr.ifr_addr;
+    
+//    printf("addr = %s\n", inet_ntoa(sa->sin_addr));
+  
+
+
+    int listener;
+    struct sockaddr_in addr;
+/*
+    char buf[1024];
+    int bytes_read;
+*/
+
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if(listener < 0)
+    {
+        perror("socket");
+        return 0;
+    }
+    
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(27015);
+    addr.sin_addr.s_addr = inet_addr(inet_ntoa(sa->sin_addr));
+    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        return 0;
+    }
+
+    listen(listener, 1);
+    
+    
+        sock = accept(listener, NULL, NULL);
+        if(sock < 0)
+        {
+            perror("accept");
+            return 0;
+        }
+/*
+        while(1)
+        {
+            bytes_read = recv(sock, buf, 1024, 0);
+            if(bytes_read <= 0) break;
+            send(sock, buf, bytes_read, 0);
+	    printf("addr = %s\n", buf);
+        }
+    
+        close(sock);
+    }
+ */   
+    return 1;
+
+
+}
+
+
+
+bool NETWORK_PLAYER::create_Line()
+{
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strcpy(ifr.ifr_name, "eth0");
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    ioctl(sock, SIOCGIFADDR, &ifr);
+    close(sock);
+    struct sockaddr_in *sa = (struct sockaddr_in*)&ifr.ifr_addr;
+    printf("addr = %s\n", inet_ntoa(sa->sin_addr));
+    struct sockaddr_in addr;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        perror("socket");
+        return 0;
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(27015);
+    addr.sin_addr.s_addr = inet_addr( inet_ntoa(sa->sin_addr));
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        return 0;
+    }
+/*
+    send(sock, message, sizeof(message), 0);
+    recv(sock, buf, sizeof(message), 0);
+    close(sock);
+    printf("addr = %s\n", buf);
+*/
+    return 1;
+}
--- network.h
+++ network.h
@@ -1,13 +1,37 @@
 #ifndef _NETWORK_H_
 	#define _NETWORK_H_
-	
 	#include <iostream>
+	#include "board.h"
 	#include "player.h"
-	
+	#include <unistd.h>
+	#include <sys/ioctl.h>
+	#include <net/if.h>
+	#include <arpa/inet.h>
+	#include <stdio.h>
+	#include <string.h>
 	class NETWORK_PLAYER: public PLAYER {
+		bool create_Server();
+		bool create_Line();
+		int sock;
 	public:
+		NETWORK_PLAYER(){
+                type = NONE;
+		char res[2];
+                std::cout << "do you wont create server game [y/n]:"; std::cin >>res;
+		switch (res[1]) {
+			case 'y':
+				if(!create_Server())exit(1);
+			case 'n':
+				if(!create_Line())exit(2);
+			default:
+				exit(3);
+		}
+		
+
+		}
 		// chooses partial half-move
 		virtual MOVE getMove(BOARD board);
+		
 	};
 	
 #endif
