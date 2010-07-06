71e298a02ca858eec5757343f3be86255c00c827
vkirkizh <vkirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-06 19:24:56 +0000

    some changes

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -2,14 +2,20 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include "board_test.h"
 
-CPPUNIT_TEST_SUITE_REGISTRATION(board_test);
+#define DEBUG
+
+#ifdef DEBUG
+	CPPUNIT_TEST_SUITE_REGISTRATION(board_test);
+#endif
 
 // entry point
 int main () {
+#ifdef DEBUG
 	CppUnit::TextUi::TestRunner runner;
 	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
 	runner.addTest(registry.makeTest());
 	runner.run();
+#endif
 	GAME shashki(HUMAN, AI);
 	return 0;
 }
