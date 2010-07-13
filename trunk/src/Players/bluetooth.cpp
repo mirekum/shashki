ac8582be673bc8b8c5b00c6ba394b45ad7d3a06a
v.kirkizh <v.kirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-13 12:11:25 +0000

    added bluetooth player interface

---
+++ trunk/src/Players/bluetooth.cpp
@@ -0,0 +1,8 @@
+#include "Players/bluetooth.h"
+
+// choose partial half-move
+MOVE Bluetooth_Player::getMove(BOARD board) {
+	MOVE result;
+	// TODO: function body
+	return result;
+}
---
+++ trunk/src/Players/bluetooth.h
@@ -0,0 +1,15 @@
+#ifndef _BLUETOOTH_H_
+	#define _BLUETOOTH_H_
+	
+	#include "Players/player.h"
+	
+	class Bluetooth_Player: public Player {
+	public:
+		// get player type
+		virtual PLAYER_TYPE type() {return BLUETOOTH;};
+		// choose partial half-move
+		virtual MOVE getMove(BOARD board);
+	};
+	
+#endif
+
