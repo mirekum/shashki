2949d42ccd164d5aae6a27a84a9a88d134f3a42b
vkirkizh <vkirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-14 19:53:50 +0000

    some changes

---
+++ trunk/src/control.h
@@ -0,0 +1,21 @@
+#ifndef _CONTROL_H_
+	#define _CONTROL_H_
+	
+	#include <QtCore>
+	#include "View/view_main.h"
+	#include "Players/player.h"
+	#include "Model/game.h"
+	
+	class Control: public QObject {
+		Q_OBJECT
+	protected:
+		View_Main *main_ui;
+		Game *game;
+	public:
+		Control(View_Main &_main_ui, Game &_game);
+	public slots:
+		void createGame(PLAYER_SETTINGS wSet, PLAYER_SETTINGS bSet);
+	};
+	
+#endif
+
