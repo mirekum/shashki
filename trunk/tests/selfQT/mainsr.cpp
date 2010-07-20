76ab2e22adf396098b55086eae09a4407e3f7d6c
k3byh83w2pdcvw@mail.ru <k3byh83w2pdcvw@mail.ru@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-20 12:24:29 +0000

    Y

---
+++ trunk/tests/selfQT/mainsr.cpp
@@ -0,0 +1,40 @@
+#include <QtGui/QApplication>
+#include "network_player.h"
+#include <stdio.h>
+#include <QtNetwork>
+	#include <iostream>
+#include <QtGui>
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+ 
+	Network_Player serv;
+
+	serv.setSelfIp("127.0.0.0");
+	serv.setColor(BLACK);
+	if(serv.createServer()==0){
+
+	MOVE lastMove[Player::maxFiguresNumber];
+	lastMove[4].from.x=1;
+	std::cout << "inter" << std::endl;
+	int f;
+	std::cin >>f;
+	if(f==0)
+	serv.giveLastMoves(lastMove);
+	printf("STOP");
+
+	}
+
+//	QList<QString> list;
+//	list=serv.getActivInterfase();
+//	list=serv.getList();
+//	foreach (QString one, list)
+///	printf((one+"\n").toStdString().c_str());
+	QLabel lbl("Hello, World !");
+	lbl.show();
+	return app.exec();
+
+
+
+
+}
---
+++ trunk/tests/selfQT/player.h
@@ -0,0 +1,31 @@
+#ifndef _PLAYER_H_
+	#define _PLAYER_H_
+	
+	#include <QtCore>
+	#include "board.h"
+	
+	enum PLAYER_TYPE {HUMAN, AI, NETWORK, BLUETOOTH};
+	
+	class Player: public QObject {
+		Q_OBJECT
+	public:
+		const static unsigned int maxFiguresNumber = BOARD::size*2;
+	protected:
+		// player color
+		COLOR color;
+	public:
+		Player() {color = NONE;}
+		// sets player color (white or black)
+		void setColor(COLOR _color) {color = _color;};
+		// get player type
+		virtual PLAYER_TYPE type() = 0;
+		// choose partial half-move
+		virtual MOVE getMove(BOARD board) = 0;
+		// set player level (for AI players)
+		virtual void setLevel(int level) {};
+		// give last moves array to player
+		virtual void giveLastMoves(MOVE lastMove[]) {};
+	};
+	
+#endif
+
