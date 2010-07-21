1ad13cf8c23d9711102f24432ec7b110b2f3a3b5
v.kirkizh <v.kirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-21 12:38:27 +0000

    View_Board made!

--- trunk/src/View/view_board.cpp
+++ trunk/src/View/view_board.cpp
@@ -16,7 +16,7 @@ void View_Board::init(Game *_game) {
 	// draw board wrapper
 	canvas = new Board_Widget(window);
 	canvas->init(game);
-	canvas->setGeometry(10, 10, 396, 396);
+	canvas->setGeometry(10, 10, 400, 400);
 	canvas->show();
 }
 
@@ -27,15 +27,43 @@ void Board_Widget::init(Game *_game) {
 
 void Board_Widget::paintEvent(QPaintEvent *event) {
 	QPainter paint(this);
-	paint.setBrush(QBrush(Qt::blue));
-	paint.drawRect(QRect(10, 10, 44, 44));
-	qDebug() << "Board_Widget::paintEvent";
+	// draw coordinate lines
+	paint.setPen(QPen(Qt::white, 1));
+	paint.setBrush(QBrush(QColor(0x55, 0x55, 0x55)));
+	for (int i = 1; i <= 8; i++) {
+		paint.drawRect(QRect(i * 44, 0, 44, 44));
+	}
+	for (int j = 1; j <= 8; j++) {
+		paint.drawRect(QRect(0, j * 44, 44, 44));
+	}
+	// draw cells
+	paint.setPen(QPen(Qt::white, 1));
+	for (int i = 0, x0 = 44; i < 8; i++) {
+		for (int j = 0, y0 = 44; j < 8; j++) {
+			paint.setBrush(QBrush(( (i + j) % 2 ? QColor(0x77, 0xdd, 0x77) : QColor(0xba, 0xdb, 0xad) )));
+			paint.drawRect(QRect(x0 + i * 44, y0 + j * 44, 44, 44));
+		}
+	}
+	// draw figures
+	for (int i = 0, x0 = 44; i < 8; i++) {
+		for (int j = 0, y0 = 44; j < 8; j++) {
+			FIGURE f = board->gcell(i, j);
+			if (IS_EMPTY(f)) continue;
+			paint.setBrush(QBrush(IS_WHITE(f) ? Qt::white : Qt::black));
+			paint.setPen(QPen(IS_WHITE(f) ? Qt::black : Qt::white, 1));
+			paint.drawEllipse(QRect(x0 + i * 44 + 4, y0 + j * 44 + 4, 36, 36));
+			if (IS_KNG(f)) {
+				paint.setPen(QPen(IS_WHITE(f) ? Qt::black : Qt::white, 3));
+				paint.drawEllipse(QRect(x0 + i * 44 + 10, y0 + j * 44 + 10, 24, 24));
+			}
+		}
+	}
 }
 
 // request move from human
 void View_Board::execMove(BOARD board) {
 	qDebug() << "View_Board::execMove";
-	sleep(10);
+	
 	emit moveExecuted();
 }
 
