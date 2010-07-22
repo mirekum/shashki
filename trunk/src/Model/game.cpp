1e3c3d55d05c3c157278f1029d454c5b38e58cdd
v.kirkizh <v.kirkizh@ee43144e-2bc8-11df-a4b8-f77f9512071c>
2010-07-22 10:28:54 +0000

    ...

--- trunk/src/Model/game.cpp
+++ trunk/src/Model/game.cpp
@@ -43,7 +43,6 @@ void getMoveThread::run() {
 }
 
 void Game::finish(GAMESTATE res_flag) {
-	qDebug() << "game finished:" << res_flag;
 	wp->giveLastMoves(lastMove);
 	bp->giveLastMoves(lastMove);
 	emit finishGame(res_flag);
